cc2af7fb512494660d6a54c02959036ef9d16161
TaLea Carpenter <taleacarpenter@gmail.com>
2018-05-23 21:42:08 -0700

    main components and express server

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <footer>
+        <div className="wrapper fixed-bottom">
+          Footer holding social media logo buttons
+        </div>
+        </footer>
+    );
+  }
+}
