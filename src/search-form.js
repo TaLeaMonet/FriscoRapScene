896cc808e64b108e885ece80942506f48bb512e9
TaLea Carpenter <taleacarpenter@gmail.com>
2018-06-28 15:13:54 -0700

    refactoring search feature component.

--- src/search-form.js
+++ src/search-form.js
@@ -3,47 +3,10 @@ import Search from './Search'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 export default class SearchForm extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-      results: []
-    };
-  }
-
-  handleSubmission = (e) => {
-    let search = this.props.match.params.input
-    console.log(this.props);
-    fetch('http://localhost:8080/search?query=' + search, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json'
-    }
-  })
-    .then(res => {
-      console.log(res);
-      return res.json()
-    })
-    .then(res => {
-      console.log(res);
-      this.setState({results: res})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    let results = {
-        search: document.getElementById('query').value,
-
-    }
-    e.preventDefault();
-
-  }
-
   render() {
     return (
-
     <div className="search-form">
-    <form className="navbar-form navbar-right" onSubmit={this.handleSubmission}>
+    <form className="navbar-form navbar-right">
       <div className="form-group">
         <input type="text" name="query" id="query"
         className="form-control"
@@ -52,8 +15,6 @@ export default class SearchForm extends Component {
       <Link to="/search" type="submit" className="btn btn-default">Submit</Link>
     </form>
     </div>
-
-
     );
   }
 }
